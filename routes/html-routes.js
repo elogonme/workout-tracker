0b5cc3d745dfe3746c373e75cf0f4672eaa105cc
Eldar <elogon@gmail.com>
2021-02-09 13:31:51 -0500

    Add icons anf fine tune styling

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 router.get('/exercise', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
