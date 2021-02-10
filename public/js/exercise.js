a2e9ca321f0fc766c21b86a562cc55c0935a5a42
Eldar <elogon@gmail.com>
2021-02-09 21:10:43 -0500

    Add last workout log to main page

--- public/js/exercise.js
+++ public/js/exercise.js
@@ -137,6 +137,8 @@ function clearInputs() {
   validateInputs();
 }
 
+
+
 if (workoutTypeSelect) {
   workoutTypeSelect.addEventListener('change', handleWorkoutTypeChange);
 }
--- public/js/workout.js
+++ public/js/workout.js
@@ -12,8 +12,8 @@ async function initWorkout() {
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises),
     };
-
     renderWorkoutSummary(workoutSummary);
+    renderLastWorkout(lastWorkout);
   } else {
     renderNoWorkoutText();
   }
@@ -79,6 +79,24 @@ function renderNoWorkoutText() {
 
   p.appendChild(strong);
   container.appendChild(p);
+}
+
+function renderLastWorkout(lastWorkoutData) {
+  const lastWorkout = document.querySelector('.last-workout');
+  const exTable = document.querySelector('#ex-list');
+  lastWorkout.classList.remove('d-none');
+  lastWorkoutData.exercises.forEach( ex => {
+    const tr = document.createElement('tr');
+    const tableRowHtml = `
+    <td data-label="Type">${ex.type}</td>
+    <td data-label="Exercise">${ex.name}</td>
+    <td data-label="Sets">${ex.sets || '-'}</td>
+    <td data-label="Reps">${ex.reps || '-'}</td>
+    <td data-label="Weight">${ex.weight || ex.distance}</td>
+    <td data-label="Duration">${ex.duration}</td>`;
+    tr.innerHTML = tableRowHtml;
+    exTable.append(tr);
+  });
 }
 
 initWorkout();
